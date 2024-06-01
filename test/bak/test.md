b4b27f8d17f692d87f3afcdaa80e0be45dce01f7
Huffer342-WSH <718007138@qq.com>
2024-06-01 09:44:36 +0800

    new test

--- test/bak/test.md
+++ test/bak/test.md
@@ -1,5 +1,11 @@
-![gif](./assets/gif.gif)
+<!-- ![gif](./assets/gif.gif) -->
+![gif](./assets/gif.webp)
 
-![a](./assets/png.png)
 
-![b](./assets/jpg.jpg)
+<!-- ![a](./assets/png.png) -->
+![a](./assets/png.webp)
+
+<!--   ![b](./assets/jpg.jpg) -->
+![b](./assets/jpg.webp)
+
+./assets/jpg.jpg
--- test/test.sh
+++ test/test.sh
@@ -3,11 +3,15 @@ script_dir=$(dirname $(readlink -f $0))
 
 cd $script_dir
 
+rm -rf ./test
+
 # 复制文件
 mkdir test
 cp -r ./bak/* ./test
+mkdir test2
+cp -r ./bak/* ./test2
 
 # 测试脚本
-# python ../src/cwebp4md.py ./test/test.md -d ./test/d/ -r ./test/r/
+python ../src/cwebp4md.py ./test/test.md -d ./test/d/ -r ./test/r/
 
-../dist/cwebp4md.exe ./test/test.md -d ./test/d/ -r ./test/r/
+../dist/cwebp4md.exe ./test2/test.md -d ./test2/d/ -r ./test2/r/
